47a4093d3873a524cc3e1a348bf199aeccb63646
Annie1790 <budayaniko@outlook.com>
2023-01-25 16:14:11 +0000

    form added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const addContainer = document.getElementById("add-container")
+const wineName = document.getElementById("name");
+const winery = document.getElementById("winery");
+const vintage = document.getElementById("vintage");
+const region = document.getElementById("region");
+const country = document.getElementById("country");
+const addNew = document.getElementById("addNew");
+
+let myLibrary = [];
+
+class Library {
+    constructor() {
+        this.name = wineName;
+        this.winery = winery;
+        this.vintage = vintage;
+        this.region = region;
+        this.country = country;
+    }
+
+    get userInput() {
+        //get inputbox.value
+    }
+}
+
+function test(form) {
+    let inputValue = form.name.value;
+    console.log(`${inputValue} received`)
+}
