4ffc1ffa9b4926f85d4b5ec23497dbf56669f209
Annie1790 <budayaniko@outlook.com>
2023-01-28 20:11:57 +0000

    search engine works

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ const addNewProduct = document.getElementById("addNewProduct");
 const addCardNode = document.getElementById("addContainer")
 const productImage = document.getElementsByClassName("card-img-top p-2");
 
+class Wine {
+    constructor(wineName,winery,vintage,region,country) {
+        this.wineName = wineName;
+        this.winery = winery;
+        this.vintage = vintage;
+        this.region = region;
+        this.country = country
+    }
+}
+
 let wineArray = [];
+let valueOfFetch;
+
+let showArrayItems = () => {
+    if (wineArray.length > 0) {
+        wineArray.forEach((item) => {
+            createProductCard(item)
+        })
+    }
+}
 
 let getDataFromForm = () => {
     const wineName = addNewProduct.name.value;
@@ -11,13 +30,26 @@ let getDataFromForm = () => {
     const region = addNewProduct.region.value;
     const country = addNewProduct.country.value;
 
-    return {
-        wineName, winery, vintage, region, country
-    }
+    return new Wine(wineName, winery, vintage, region, country)
 }
 
-let createProductCard = () => {
-    getDataFromForm();
+let fetchGoogleImage = (wineImgSrc, wineName, wineVintage) => {
+    fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCk9ieYVeOJzdx06_70PwBZywfOCgKVZ0o&cx=d669cff582af647b3&q=${wineName}%20${wineVintage}&searchType=image`,
+        {
+            mode: "cors",
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then((response) => response.json()
+        .then((value) => {
+            wineImgSrc.src = value.items[0].link;
+            
+        }))
+}
+
+
+let createProductCard = (wine) => {
 
     let cardDiv = document.createElement("div");
     cardDiv.className = "card";
@@ -25,14 +57,15 @@ let createProductCard = () => {
 
 
     let imgHold = document.createElement("img");
+    imgHold.style.objectFit = "cover";
     imgHold.className = "card-img-top p-2";
-    //imgSrc missing
+    fetchGoogleImage(imgHold, wine.wineName, wine.vintage);
 
     let cardBody = document.createElement("div");
     cardBody.className = "card-body";
 
     let title = document.createElement("h5");
-    title.innerHTML = getDataFromForm().wineName;
+    title.innerHTML = wine.wineName;
     title.className = "card-title";
 
     let button = document.createElement("input");
@@ -47,28 +80,17 @@ let createProductCard = () => {
     cardBody.appendChild(button);
 }
 
-let fetchGoogleImage = () => {
-    fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCk9ieYVeOJzdx06_70PwBZywfOCgKVZ0o&cx=d669cff582af647b3&q=szepsyhttps://www.googleapis.com/customsearch/v1?key=AIzaSyCk9ieYVeOJzdx06_70PwBZywfOCgKVZ0o&cx=d669cff582af647b3&q=${getDataFromForm().wineName}&searchType=image`,
-        {
-            mode: "cors",
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-        .then((response) => response.json()
-        .then((value) => console.log(value)))
-}
 
-let getImgSrc = () => {
-
-}
 
 //API Key : AIzaSyCk9ieYVeOJzdx06_70PwBZywfOCgKVZ0o
 //engine id/cx : d669cff582af647b3
 
+window.addEventListener("load", function() {
+    showArrayItems();
+})
 
 addNewProduct.addEventListener("submit", function (e) {
-    wineArray.push(getDataFromForm())
     e.preventDefault();
-    createProductCard();
+    wineArray.push(getDataFromForm());
+    createProductCard(getDataFromForm());
 })
